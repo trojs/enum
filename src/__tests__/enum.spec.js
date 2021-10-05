7ae0f7e2731cffb5eede7cbbdaa5ae6dd7374052
Pieter Wigboldus <w3n@w3news.org>
2021-10-05 09:17:23 +0200

    Finetune the tests

--- src/__tests__/enum.spec.js
+++ src/__tests__/enum.spec.js
@@ -93,26 +93,26 @@ describe('Test the encoding enum', () => {
       value: 1
     })
     expect(example.keys).toEqual(['value'])
-    expect(example.key).toEqual('value')
+    expect(example.value).toEqual(1)
     expect(example.length).toEqual(1)
   })
 
   it('It should handle reserved word options', () => {
     class Example extends Enum {
         static options = {
-          options: 1
+          options: 42
         }
     }
     const example = Example.fromKey('options')
 
     expect(example.key).toEqual('options')
-    expect(example.value).toEqual(1)
-    expect(example.values).toEqual([1])
+    expect(example.value).toEqual(42)
+    expect(example.values).toEqual([42])
     expect(Example.options).toEqual({
-      options: 1
+      options: 42
     })
     expect(example.keys).toEqual(['options'])
-    expect(example.key).toEqual('options')
+    expect(example.options).toEqual(42)
     expect(example.length).toEqual(1)
   })
 
@@ -144,24 +144,24 @@ describe('Test the encoding enum', () => {
           options: 1
         }
     }
-    const example = Example.fromKey('options')
-    expect(example.values).toEqual([1])
     expect(Example.options).toEqual({
       options: 1
     })
+    const example = Example.fromKey('options')
+    expect(example.values).toEqual([1])
 
     Example.options.options = 42
-    expect(example.values).toEqual([42])
     expect(Example.options).toEqual({
       options: 42
     })
+    expect(example.values).toEqual([42])
 
     Example.options = {
       options2: 2
     }
-    expect(example.values).toEqual([2])
     expect(Example.options).toEqual({
       options2: 2
     })
+    expect(example.values).toEqual([2])
   })
 })
